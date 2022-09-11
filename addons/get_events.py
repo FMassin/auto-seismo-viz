14ed0dff73db427db9632763e2ea2bf4c8b4569c
Frédérick Massin <fmassin@sed.ethz.ch>
2022-09-11 14:57:37 +0200

    Merge event informations, adds event xml artifact

---
+++ addons/get_events.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python
+from obspy import read_events
+from obspy.core.event.catalog import Catalog
+from obspy.clients.fdsn.header import FDSNNoDataException
+import numpy
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    #lon1 = numpy.deg2rad(lon1)
+    #lat1 = numpy.deg2rad(lat1)
+    #lon2 = numpy.deg2rad(lon2)
+    #lat2 = numpy.deg2rad(lat2)
+    lon1, lat1, lon2, lat2 = map(numpy.deg2rad, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = numpy.sin(dlat/2)**2 + numpy.cos(lat1) * numpy.cos(lat2) * numpy.sin(dlon/2)**2
+    c = 2 * numpy.arcsin(numpy.sqrt(a))
+    r = 6371000 # Radius of earth in meters. Use 3956 for miles
+    return c * r
+
+def match_events(self=Catalog(),
+                 toadd=Catalog(),
+                 client=None,
+                 v=False,
+                 x=True,
+                 lastmagpref=True,
+                 **get_events_args):
+    """
+        Matchs the events in self (obspy:Catalog) with specified catalog.
+
+        Matchs with catalog available in webservice if client is specified.
+    """
+
+    tofind = toadd.copy()
+    matchs = Catalog()
+    extras = Catalog()
+    matchs.description = 'Intersections of '+str(self.description)+' and '+str(tofind.description)
+    missed = Catalog()
+    missed.description = 'Part of '+str(self.description)+' not in '+str(tofind.description)
+    listfound=list()
+    
+    if v:
+        if client:
+            print('Reference client')
+            print(client)
+
+    for e in self.events :
+
+        o = e.preferred_origin() 
+        if o is None:
+            o = e.origins[-1]
+
+
+        found=False
+        memdl=99999999
+        memdt=99999999
+        if v:
+            print('---- event',e.short_str(),'...')
+
+        if len(listfound) < len(self.events):
+
+            filter = ["time >= "+str(o.time-120),
+                      "time <= "+str(o.time+120),
+                      "latitude >= "+str(o.latitude-2.5),
+                      "latitude <= "+str(o.latitude+2.5),
+                      "longitude >= "+str(o.longitude-2.5),
+                      "longitude <= "+str(o.longitude+2.5)]
+
+            if client :
+                eq_specs = {'starttime':str(o.time-120),
+                            'endtime':str(o.time+120),
+                            'minlatitude':max([-90,o.latitude-2.5]),
+                            'maxlatitude':min([90, o.latitude+2.5]),
+                            'minlongitude':max([-180,o.longitude-2.5]),
+                            'maxlongitude':min([180,o.longitude+2.5])}
+                try:
+                    tofind = client.get_events( **eq_specs)
+                    if v:
+                        print('Event in reference client')
+                        print(tofind)
+                except:
+                    if v:
+                        print('No event in reference client')
+                    continue
+                if len(tofind.events)==0:
+                    try:
+                        tofind = read_events(get_events_args['filename'],
+                                                   format=get_events_args['format'].upper())
+                    except:
+                        if v:
+                            print('can t read filename')
+                            print(get_events_args['filename'])
+                            return
+                        pass
+            if v:
+                print(tofind)
+                print(filter)
+            for candidate in tofind.filter( *filter ).events:
+                if v:
+                    print(candidate)
+                candidateo = candidate.preferred_origin()
+                if o is  None:
+                    candidateo = candidate.origins[-1]
+
+                if candidateo.time not in listfound:
+
+                    dt = abs(o.time-candidateo.time)
+                    dl = haversine(o.longitude,
+                                                    o.latitude,
+                                                    candidateo.longitude,
+                                                    candidateo.latitude)
+                    if v:
+                        print(dt,dl)
+                    if (dt < 10 and dl <=50000 and
+                        (dl<memdl or dt<memdt)):
+
+                        found = True
+                        memi = str(candidateo.time)
+                        memdl = dl
+                        memdt = dt
+                        meme = candidate
+                        if v:
+                            print('fits nicely input catalog ',tofind.description,':\n  ', candidate.short_str())
+
+                        break
+
+                    elif (dt < 40 and dl <=100000 and
+                          (dl<memdl or dt<memdt)):
+
+                        found = True
+                        memi = str(candidateo.time)
+                        memdl = dl
+                        memdt = dt
+                        meme = candidate
+                        if v:
+                            print('fits input catalog ',tofind.description,':\n  ', candidate.short_str())
+
+                    elif (dt < 80 and dl <200000 and
+                          (dl<memdl or dt<memdt)):
+
+                        found = True
+                        memi = str(candidateo.time)
+                        memdl = dl
+                        memdt = dt
+                        meme = candidate
+                        if v:
+                            print('poorly fits input catalog ',tofind.description,':\n  ', candidate.short_str())
+
+
+        if not found:
+            if v:
+                print('does not exist in current catalog')
+            missed.events.append(e)
+
+        elif found:
+            if v:
+                print('merged with ', meme.short_str())
+            #matchs.events.append(e)
+            o = meme.preferred_origin()
+            if o is  None:
+                o = meme.origins[-1]
+
+            
+            if client is not None:
+                meme = client.get_events(starttime=str(o.time-1),
+                                        endtime=str(o.time+1),
+                                        **get_events_args )
+
+                if 'filename' in get_events_args.keys():
+                    f = open(get_events_args['filename'],'r')
+                    filedata = f.read()
+                    f.close()
+
+                    newdata = filedata.replace('0.10" version="0.10"','0.9" version="0.9"')
+                    newdata = filedata.replace('0.11" version="0.11"','0.9" version="0.9"')
+                    newdata = filedata.replace('0.12" version="0.12"','0.9" version="0.9"')
+
+                    f = open(get_events_args['filename'],'w')
+                    f.write(newdata)
+                    f.close()
+
+                    meme = read_events(get_events_args['filename'],
+                                            format=get_events_args['format'].upper())
+
+                meme=meme[0]
+
+            matchs.events.append(meme)
+            listfound.append(memi)
+
+            #for prefatt in [ 'preferred_origin_id', 'preferred_magnitude_id' ]:
+            #    if hasattr(meme, prefatt):
+            #        matchs.events[-1][prefatt] = meme[prefatt]
+
+            print(e)
+            for listatt in [ 'origins' , 'magnitudes', 'picks', 'station_magnitudes','focal_mechanisms' ]:
+            #    if hasattr(meme, listatt):
+            #        matchs.events[-1][listatt].extend( meme[listatt] )
+
+                if hasattr(e, listatt):
+                    for i,att in enumerate(e[listatt] ):
+                        matchs.events[-1][listatt].insert(i,att)
+
+            if False:
+                geocode = geocoder.osm([matchs.events[-1].preferred_origin().latitude,
+                                        matchs.events[-1].preferred_origin().longitude],
+                                       method='reverse').json
+                obspytypes = ["region name","nearest cities"]
+                for itype,type in enumerate(['region','city']):
+                    matchs.events[-1].event_descriptions.append(obspy.core.event.event.EventDescription(text=geocode[type],
+                                                                                                    type=obspytypes[itype]))
+
+
+
+            if matchs.events[-1].preferred_magnitude() is None:
+                print('No pref mag')
+                print(matchs.events[-1])
+                continue
+
+            if not hasattr(matchs.events[-1].preferred_magnitude(), 'origin_id'):
+                matchs.events[-1].preferred_magnitude().origin_id=matchs.events[-1].preferred_origin().resource_id
+
+            if matchs.events[-1].preferred_magnitude().origin_id is None:
+                matchs.events[-1].preferred_magnitude().origin_id=matchs.events[-1].preferred_origin().resource_id
+
+            if False:
+                matchs.events[-1].preferred_origin=matchs.events[-1].preferred_origin_id.get_referred_object
+                matchs.events[-1].preferred_magnitude=matchs.events[-1].preferred_magnitude_id.get_referred_object
+                if matchs.events[-1].preferred_focal_mechanism_id is not None:
+                    matchs.events[-1].preferred_focal_mechanism=matchs.events[-1].preferred_focal_mechanism_id.get_referred_object
+
+            #tofind.events.remove(meme)
+    if lastmagpref:
+        for e in matchs:
+            e.preferred_origin_id = e.origins[-1].resource_id
+            e.preferred_magnitude_id = e.magnitudes[-1].resource_id
+    if x :
+        matchs, extras, trash = match_events(self=tofind,
+                                                          toadd=self,
+                                                          client=client,
+                                                          v=v,
+                                                          x=False)
+
+    return matchs, missed, extras
+
+
+def get_events_updatedafter(self,
+                            updated_after=None,
+                            debug=True,
+                            **kwargs):
+
+    if debug:
+        print('//////// get_events_updatedafter ////////')
+    refclient = self
+    if isinstance(self,list):
+        if debug:
+            print('List of clients provided')
+        refclient=self[0]
+
+    ## The catalog in last N days
+    catalog_last_ndays = Catalog()
+    try:
+        catalog_last_ndays += refclient.get_events(**kwargs)
+    except FDSNNoDataException:
+        if debug:
+            print('No event found with', kwargs)
+
+    if debug:
+        print('Found s:')    
+        print(catalog_last_ndays.__str__(print_all=True))
+
+    ## The catalog in last N days updated in last N seconds
+    catalog_updated_last_nseconds = Catalog()
+    for e in catalog_last_ndays:
+        for m in e.magnitudes:
+            if m.creation_info.creation_time > updated_after:
+                eventid=str(e.resource_id)
+                if not 'smi:' == eventid[:4]:
+                    eventid = str(e.resource_id).split("?")[-1]
+                    if '&' in eventid:
+                        eventid=[p.split('=')[-1] for p in eventid.split('&') if 'eventid' in p][0]
+                    
+                    eventid = eventid.split("/")[-1]
+
+                try:
+                    allsolutions_noarrivals = refclient.get_events(eventid=eventid,
+                                                                  includeallmagnitudes=True,
+                                                                  includeallorigins=True,
+                                                                  includearrivals=False)
+                    prefsolution_witharrivals = refclient.get_events(eventid=eventid,
+                                                                    includeallmagnitudes=False,
+                                                                    includeallorigins=False,
+                                                                    includearrivals=True)
+                
+                except FDSNNoDataException:
+                    if debug:
+                        print('No event found with eventid=', eventid)
+                    break
+
+                except:
+                    try:
+                        allsolutions_noarrivals = refclient.get_events(eventid=eventid,
+                                                                    includeallmagnitudes=True,
+                                                                    includeallorigins=True)
+                        prefsolution_witharrivals = allsolutions_noarrivals
+                        if debug:
+                            print('Cannot use includearrivals')
+
+                    except FDSNNoDataException:
+                        if debug:
+                            print('No event found with eventid =',eventid,'(includearrivals not used)')
+                        break
+                if debug:
+                    print('Event %s should be updated'%eventid)
+
+                catalog_updated_last_nseconds += prefsolution_witharrivals[0]
+                catalog_updated_last_nseconds[-1].origins = allsolutions_noarrivals[0].origins
+                catalog_updated_last_nseconds[-1].magnitudes = allsolutions_noarrivals[0].magnitudes
+
+                if not isinstance(self,list):
+                    break
+
+                for client in self:
+                    ## Get additional solution form another server if any provided
+                    if debug:
+                        print('Adding more event info with',client)
+                    catalog_updated_last_nseconds = match_events(catalog_updated_last_nseconds,
+                                                                client=client,
+                                                                lastmagpref=False,
+                                                                includeallmagnitudes=True,
+                                                                includeallorigins=True,
+                                                                includearrivals=False,
+                                                                includecomments=True,
+                                                                filename='/tmp/xml',
+                                                                format='sc3ml',
+                                                                v=debug,
+                                                                x=False)[0]
+                break
+
+    if debug:
+        print('Updated after %s:'%updated_after)    
+        print(catalog_updated_last_nseconds.__str__(print_all=True))
+
+    return catalog_updated_last_nseconds
--- seismo-viz.py
+++ seismo-viz.py
@@ -1,77 +1,88 @@
 #!/usr/bin/env python
-from obspy.core.event.catalog import Catalog
 from obspy.clients.fdsn import Client 
 from obspy.core import UTCDateTime
-from obspy.clients.fdsn.header import FDSNNoDataException
 from sys import argv
 from re import sub
 
-def main(fdsnws_uri='USGS',
+from addons.get_events import get_events_updatedafter
+
+def main(catalog_uri='USGS',
+         inventory_url=None,
+         stream_url=None,
          ndays=4,
          nseconds=15*60*60,
-         minmagnitude=7,
-         **kwargs):
+         user=None, 
+         password=None, 
+         debug=False, 
+         timeout=120, 
+         eida_token=None,
+         **get_events_wargs):
 
-    nsecongs_ago = UTCDateTime.now()-nseconds
-    myclient = Client(fdsnws_uri,**kwargs)
+    if not isinstance(catalog_uri,list):
+        catalog_uri = catalog_uri.split(',')
 
-    ## The catalog in last N days
-    catalog_last_ndays = Catalog()
-    options = kwargs
-    options['minmagnitude'] = minmagnitude
-    options['starttime'] = UTCDateTime.now()-ndays*24*60*60
-    try:
-        catalog_last_ndays += myclient.get_events(**options)
-    except FDSNNoDataException:
-        print('No event found with', options)
+    if inventory_url is None and stream_url is not None:
+        inventory_url = stream_url
+    elif inventory_url is None :
+        inventory_url = catalog_uri[0]
 
-    print('Found in the last %d days:'%ndays)    
-    print(catalog_last_ndays.__str__(print_all=True))
+    if  stream_url is None and  inventory_url is not None:
+        stream_url = inventory_url
+    elif stream_url is None :
+        stream_url = catalog_uri[0]
 
-    ## The catalog in last N days updated in last N seconds
-    catalog_updated_last_nseconds = Catalog()
-    for e in catalog_last_ndays:
-        for m in e.magnitudes:
-            if m.creation_info.creation_time > nsecongs_ago:
-                eventid = str(e.resource_id).split("?")[-1]
-                if '&' in eventid:
-                    eventid=[p.split('=')[-1] for p in eventid.split('&') if 'eventid' in p][0]
-                
-                eventid = eventid.split("/")[-1]
+    if ndays is not None :
+        ndays = float(ndays)
 
-                try:
-                    allsolutions_noarrivals = myclient.get_events(eventid=eventid,
-                                                                  includeallmagnitudes=True,
-                                                                  includeallorigins=True,
-                                                                  includearrivals=False)
-                    prefsolution_witharrivals = myclient.get_events(eventid=eventid,
-                                                                    includeallmagnitudes=False,
-                                                                    includeallorigins=False,
-                                                                    includearrivals=True)
-                    print('Event %s should be updated'%eventid)
-                except:
-                    try:
-                        allsolutions_noarrivals = myclient.get_events(eventid=eventid,
-                                                                    includeallmagnitudes=True,
-                                                                    includeallorigins=True)
-                        prefsolution_witharrivals = allsolutions_noarrivals
-                        print('Cannot use includearrivals but event %s should be updated'%eventid)
-                    except FDSNNoDataException:
-                        print('No event found with eventid =',eventid)
-                        break
+    if nseconds is not None :
+        nseconds = float(nseconds)
+    catalog_clients=[]
+    for url in catalog_uri:
+        catalog_clients.append(Client(base_url=url,
+                                user=user, 
+                                password=password, 
+                                debug=debug, 
+                                timeout=timeout, 
+                                eida_token=eida_token))
 
-                catalog_updated_last_nseconds += prefsolution_witharrivals[0]
-                catalog_updated_last_nseconds[-1].origins = allsolutions_noarrivals[0].origins
-                catalog_updated_last_nseconds[-1].magnitudes = allsolutions_noarrivals[0].magnitudes
-                break
+    updated_after = UTCDateTime.now()-nseconds
+    get_events_wargs['starttime'] = UTCDateTime.now()-ndays*24*60*60
 
-    print('Updated in the last %d seconds:'%nseconds)    
-    print(catalog_updated_last_nseconds.__str__(print_all=True))
+    catalog = get_events_updatedafter(catalog_clients,
+                                      updated_after=updated_after,
+                                      **get_events_wargs)
+
+    for event in catalog:
+        
+        ## Save Event xml
+        event.write('data/%s.quakeml'%str(event.resource_id).split('/')[-1],
+                    format='quakeml')
+
+        ## Get seismic (meta)data
+        #
+
+        ## Plot data
+        #
+
+        ## Map results
+        #
+
+        ## Plot results timeline
+        #
+
+        ## Animate data and results
+        #
 
 if __name__ == '__main__':
 
     args={ arg.split('=')[0]:arg.split('=')[1] for arg in argv[1:] if "=" in arg}
+    tmp='\n'.join(['%s: %s'%(arg,args[arg]) for arg in args])
+    tmp=sub('catalog_uri: .*\n',  'catalog_uri: *****\n',tmp)
+    tmp=sub('stream_url: .*\n',   'stream_url: *****\n',tmp)
+    tmp=sub('inventory_url: .*\n','inventory_url: *****\n',tmp)
+    tmp=sub('user:.*\n',       'user: *****\n',      tmp)
+    tmp=sub('password:.*\n',   'password: *****\n',  tmp)
     print('Arguments provided:')
-    print(sub('/.*:.*/','/*hiden*:*hiden*/','\n'.join(['%s: %s'%(arg,args[arg]) for arg in args])))
+    print(tmp)
 
     main(**args)
